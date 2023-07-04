c66d1efd3c31678cff6a53c2f286c9e9374be38d
bram <brammahendrawan693@gmail.com>
2023-07-05 01:35:48 +0700

    create api books detail
    Signed-off-by: bram <brammahendrawan693@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 const {
     addBookHandler,
+    getAllBooksHandler,
 } = require('./handler');
 
 const routes = [{
-    method: 'POST',
-    path: '/books',
-    handler: addBookHandler,
-}];
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
+    },
+];
 
 module.exports = routes;
